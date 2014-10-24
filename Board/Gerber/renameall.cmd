72c83f2a7e413b79a196b2799172fb59c26569b6
Kenta IDA <cinimlm@gmail.com>
2014-10-25 01:48:22 +0900

    ベースボードに半田付け時のリファレンスピン用穴を追加

--- Board/Gerber/renameall.cmd
+++ Board/Gerber/renameall.cmd
@@ -1,5 +1,6 @@
 @ECHO OFF
 MOVE /Y BL600BaseBoard.drl 		     BL600BaseBoard.txt
+MOVE /Y BL600BaseBoard-NPTH.drl 	 BL600BaseBoard-NPTH.txt
 MOVE /Y BL600BaseBoard-F_Cu.gtl      BL600BaseBoard.gtl
 MOVE /Y BL600BaseBoard-B_Cu.gbl      BL600BaseBoard.gbl
 MOVE /Y BL600BaseBoard-F_Mask.gts    BL600BaseBoard.gts
@@ -10,3 +11,4 @@ MOVE /Y BL600BaseBoard-Edge_Cuts.gbr BL600BaseBoard.gml
 MOVE /Y BL600BaseBoard-F_Paste.gtp   BL600BaseBoard.gtp
 MOVE /Y BL600BaseBoard-B_Paste.gbp   BL600BaseBoard.gbp
 
+PAUSE
